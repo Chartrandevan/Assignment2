a537650c464597fbcae86e0fec534c5eb7de5ee5
evanr <evanr@LAPTOP-3TOQ27Q2.gateway.mts.net>
2017-12-16 17:59:24 -0600

    let the user select a specific time

--- src/assignment2actual/skaterclient.java
+++ src/assignment2actual/skaterclient.java
@@ -28,6 +28,8 @@ public class skaterclient/*CHANGE THIS TO THE NAME OF THE FILE*/
 	
 	// ***** declaration of variables *****
 	
+          int timereq = 0;
+          int skaterreq = 0;
           int count = 0;
           int complist[] = null;
           int skcount = 0;
@@ -37,6 +39,14 @@ public class skaterclient/*CHANGE THIS TO THE NAME OF THE FILE*/
           String strin;  
            BufferedReader fin = new BufferedReader(new FileReader("skaterData.txt"));
            
+            String banner;               // output banner for windows
+
+        // **** create banner ****
+        banner = "*******************************************\n";
+        banner += "Name:		Evan Chartrand\n";
+        banner += "Class:		CS30S\n";
+        banner += "Assignment:	Ax Qy\n";
+        banner += "*******************************************\n\n";
            	System.out.println("**********************************");
 		System.out.println("NAME:        Your Name Here");
 		System.out.println("Class:       CS30S");
@@ -79,21 +89,35 @@ public class skaterclient/*CHANGE THIS TO THE NAME OF THE FILE*/
         skcount ++;
         strin = fin.readLine();      
         }//end while
-
         
+         String promt = "which skater would you like to see the times of"+ "\nex 3"+"\nenter -1 to cancel";
+         skaterreq = getNumber(banner, promt);
+        while(skaterreq != -1){
+         promt = "Which time would you like to view"+"\nex 7"+"\nenter -1 to cancel or select antoher skater";
+         timereq = getNumber(banner, promt);
+         while (timereq != -1){
+         System.out.print("time = " + skatearray[skaterreq-1].getcomptime()[timereq-1]/60 + ":");
+              if(skatearray[skaterreq-1].getcomptime()[timereq-1] >= 420 && skatearray[skaterreq-1].getcomptime()[timereq-1] < 430) {
+                 System.out.print("0"); 
+            }//end if
+            System.out.print(skatearray[skaterreq-1].getcomptime()[timereq-1] % 60 + " ");
+          timereq = getNumber(banner, promt);
+         }
+         promt = "which skater would you like to see the times of"+ "\nex 3"+"\nenter -1 to cancel";
+         skaterreq = getNumber(banner, promt);
+        }
 
 	// ***** closing message *****
 	
-		System.out.println("end of processing");
+		System.out.println("\nend of processing");
 	
 	}  // end main	
         
-
-    
+        
     
         public static int getNumber(String banner, String promt) {
         int n = 0;          // local varible to hold integer from keyboard buffer
-        n = Integer.parseInt(JOptionPane.showInputDialog(banner, promt));
+        n = Integer.parseInt(JOptionPane.showInputDialog(banner+ promt));
         return (n);
     } // end getNumber
 }  // end class
